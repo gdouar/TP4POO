7e3d024a7a28b30517dfd58815ec3ab82cb648c4
bzhong <benoit.zhong@insa-lyon.fr>
2017-01-10 18:01:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 using namespace std;
 #include <iostream>
-#include <stdlib>
+#include <cstdlib>
 #include <string>
 
 int main(int argc, char* argv[])
@@ -16,35 +16,54 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	for(int i=1; i< argc; i++)
+	for(int i=1; i< argc-1; i++)
 	{
-		if(argv[i] == "-e")
+		//cout << argv[i] << endl;
+		if(strcmp(argv[i], "-e") == 0)
 		{
 			enleverExtensions = true;
+			cout << "Images, CSS et Javascript files ignorés." << endl;
 		}
-		else if(argv[i] == "-t")
+		else if(strcmp(argv[i], "-t") == 0)
 		{
-			if(i+1 < argc)
+			if(i+1 < argc-1)
 			{
 				heure = atoi(argv[i+1]);
+				if(heure < 0 || heure > 23)
+				{
+					cerr << "Heure doit être compris entre 0 et 23." << endl;
+					return 1;
+				}
 				i++;
+				cout << "Heure comprise entre "<< heure << "h et " << heure+1 << "h."<<endl;
 			}
 			else
 			{
 				cerr << "-t nécessite un paramètre supplémentaire (heure)" << endl;
+				return 1;
 			}
 			
 		}
-		else if(argv[i] == "-g")
+		else if(strcmp(argv[i], "-g") == 0)
 		{
-			nomFichierGraph = str(argv[i+1]);
-			i++;
+			if(i+1 < argc-1)
+			{
+				nomFichierGraph = argv[i+1];
+				i++;
+			}
+			else
+			{
+				cerr << "-g nécessite un paramètre supplémentaire (nom du fichier)" << endl;
+				return 1;
+			}
 		}
 		else
 		{
-			nomFichierLog = str(arg[i]);
+			cerr << "Paramètre inconnu : " << argv[i] << endl;
 		}
 	}
+
+	nomFichierLog = argv[argc-1];
 
 	return 0;
 }
