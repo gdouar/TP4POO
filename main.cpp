ad26dc0f379ed3b0ab435753f08878c88b60e2b9
bzhong <benoit.zhong@insa-lyon.fr>
2017-01-10 17:42:26 +0100

    GitIgnore

--- main.cpp
+++ main.cpp
@@ -1,3 +1,50 @@
-int main(){
-	
+using namespace std;
+#include <iostream>
+#include <stdlib>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+	bool enleverExtensions = false;
+	int heure;
+	string nomFichierGraph;
+	string nomFichierLog;
+
+	if(argc < 2)
+	{
+		cerr << "Usage: " << argv[0] << " [options] nomfichier.log" << endl;
+		return 1;
+	}
+
+	for(int i=1; i< argc; i++)
+	{
+		if(argv[i] == "-e")
+		{
+			enleverExtensions = true;
+		}
+		else if(argv[i] == "-t")
+		{
+			if(i+1 < argc)
+			{
+				heure = atoi(argv[i+1]);
+				i++;
+			}
+			else
+			{
+				cerr << "-t nécessite un paramètre supplémentaire (heure)" << endl;
+			}
+			
+		}
+		else if(argv[i] == "-g")
+		{
+			nomFichierGraph = str(argv[i+1]);
+			i++;
+		}
+		else
+		{
+			nomFichierLog = str(arg[i]);
+		}
+	}
+
+	return 0;
 }
